dd22b27a4f9580b6be2e4f91663a7fb98c6f7c73
slipskid <2773262@gmail.com>
2019-11-28 17:03:01 -0500

    Add dotenv.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 'use strict';
-
+require('dotenv').config()
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
@@ -8,10 +8,10 @@ var cors = require('cors');
 
 var app = express();
 
-// Basic Configuration 
+// Basic Configuration
 var port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/ 
+/** this project needs a db !! **/
 // mongoose.connect(process.env.MONGOLAB_URI);
 
 app.use(cors());
@@ -25,13 +25,13 @@ app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
-// your first API endpoint... 
+
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
 
 app.listen(port, function () {
-  console.log('Node.js listening ...');
+  console.log('Node.js listening on port: ' + port);
 });
