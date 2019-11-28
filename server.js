63c5c162bad1327364dc3d8b87db05204559b2e7
slipskid <2773262@gmail.com>
2019-11-28 17:45:29 -0500

    Add connection to MongoDB

--- server.js
+++ server.js
@@ -1,37 +1,37 @@
-'use strict';
+'use strict'
+
 require('dotenv').config()
-var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
 
-var cors = require('cors');
+const express = require('express')
+const mongo = require('mongodb')
+const mongoose = require('mongoose')
 
-var app = express();
+const cors = require('cors')
+
+const app = express()
 
 // Basic Configuration
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
 /** this project needs a db !! **/
-// mongoose.connect(process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGODB_URI)
 
-app.use(cors());
+app.use(cors())
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/public', express.static(process.cwd() + '/public'))
 
-app.get('/', function(req, res){
-  res.sendFile(process.cwd() + '/views/index.html');
-});
-
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html')
+})
 
 // your first API endpoint...
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' })
+})
 
 app.listen(port, function () {
-  console.log('Node.js listening on port: ' + port);
-});
+  console.log('Node.js listening on port: ' + port)
+})
